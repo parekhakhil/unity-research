5750272e5cd724a435750f6c51556dce3c1e1e13
parekhakhil <39043468+parekhakhil@users.noreply.github.com>
2020-11-29 08:59:21 +0530

    init commit

---
+++ Assets/Scripts/ChaneScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChaneScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2")){
+        int thisLevel = SceneManager.GetActiveScene().buildIndex;
+        if(thisLevel < SceneManager.sceneCountInBuildSettings){
+            SceneManager.LoadScene(thisLevel + 1); 
+        }
+        else{
+        }
+            Debug.Log("Scene Count: " + SceneManager.sceneCountInBuildSettings);
+        }
+    }
+
+    public void NextScene(){
+      int thisLevel = SceneManager.GetActiveScene().buildIndex;
+        if(thisLevel < SceneManager.sceneCountInBuildSettings){
+            SceneManager.LoadScene(thisLevel + 1); 
+        }
+        else{
+        }
+            Debug.Log("Scene Count: " + SceneManager.sceneCountInBuildSettings);
+    } 
+}
---
+++ Assets/Scripts/ImageCreator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageCreator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+       
+    }
+
+    void Start()
+    {
+        // place items with random position feched through resources
+        var jsonTextFile = Resources.Load<TextAsset>("iocl");
+        // var obj = JsonUtility.FromJson<ImageCreator>(jsonTextFile);
+        // Debug.Log("json data",obj);
+        for(int i=1;i<=5;i++){
+        SpriteCreator("hotspot"+i,i+5,i+6,i+7);
+        }
+    }
+
+    public void SpriteCreator(string objectname,int xpos,int ypos,int zpos) {
+        GameObject go = new GameObject(objectname);
+        SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
+        renderer.sprite = Resources.Load<Sprite>("next-button");
+        go.transform.position = new Vector3(xpos,ypos,zpos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
